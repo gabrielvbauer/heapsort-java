58925d8e44cb3b18af9fc925f9f68917647d9416
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2021-09-17 22:15:36 -0300

    comments added

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -14,30 +14,37 @@ public class HeapSort {
     int left = 2 * num + 1;
     int right = 2 * num + 2;
 
+    /* Verificando se o filho da esquerda existe e se é maior que o seu elemento raiz */
     if (left < arrLength && arr[root] < arr[left]) {
       root = left;
     }
 
+    /* Verificando se o filho da direita existe e se é maior que o seu elemento raiz */
     if (right < arrLength && arr[root] < arr[right]) {
       root = right;
     }
 
+    /* caso algum dos elementos filhos seja maior, a troca de elementos raiz é feita */
     if (root != num) {
       int temp = arr[root];
       arr[root] = arr[num];
       arr[num] = temp;
 
+      /* Construindo um novo maxheap com as novas mudanças na árvore */
       heapify(arr, arrLength, root);
     }
   }
 
+  /* A função heapsort organiza um array definido no construtor */
   public int[] heapSort() {
     int arrLength = this.getArr().length;
 
+    /* Constrói um maxheap */
     for (int i = arrLength/2; i > -1; i--) {
       heapify(getArr(), arrLength, i);
     }
 
+    /* Extraindo os elementos um por um */
     for (int i = arrLength - 1; i > 0; i--) {
       int temp = getArr()[i];
       getArr()[i] = getArr()[0];
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@ public class Main {
   public static void main(String[] args) {
     int[] array = {10, 9, 5, 8, 4, 3, 6, 2, 1, 7};
 
-    System.out.println(Arrays.toString(array));
+    System.out.printf("""
+                      ------------------
+                      Array original: %s
+                      ------------------
+                      """, Arrays.toString(array));
     HeapSort hs = new HeapSort(array);
-    System.out.println(Arrays.toString(hs.heapSort()));
+    System.out.printf("""
+                      Array organizado: %s
+                      ------------------
+                      """, Arrays.toString(hs.heapSort()));
   }
 }
+
